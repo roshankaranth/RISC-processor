b9c698d7efd44186f6df38ce95568c410df8ec28
roshankaranth <roshankaranth@gmail.com>
2025-03-25 12:00:48 +0530

    controller

--- ALU.v
+++ ALU.v
@@ -10,6 +10,7 @@ module mux3to1(out,sel,in1,in2,in3);
             2'b00 : out = in1;
             2'b01 : out = in2;
             2'b10 : out = in3;
+            default : out = 1'bx;
         endcase
     end
 endmodule;
@@ -85,29 +86,5 @@ module ALU(in1,in2,Cin,Cout,op,binvert,out);
 
 endmodule
 
-// module tbALU();
-//     reg Binvert, Carryin;
-//     reg [1:0] Operation;
-//     reg [31:0] a,b;
-//     wire [31:0] Result;
-//     wire CarryOut;
 
-//     ALU uut(a,b,Carryin,CarryOut,Operation,Binvert,Result);
 
-//     initial
-//         $monitor("$time : %0t, a = %b , b = %b, cin = %b, cout = %b, op = %b, binvert = %b, res = %b", $time,a,b,Carryin,CarryOut,Operation,Binvert,Result);
-
-//     initial begin
-//         a=32'ha5a5a5a5;
-//         b=32'h5a5a5a5a;
-//         Operation=2'b00;
-//         Binvert=1'b0;
-//         Carryin=1'b1; //must perform AND resulting in zero
-
-//         #100 Operation=2'b01; //OR
-//         #100 Operation=2'b10; //ADD
-//         #100 Binvert=1'b1;//SUB
-
-//         #200 $finish;
-//     end
-// endmodule

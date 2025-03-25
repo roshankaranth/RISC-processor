37471e489b5f6b9e82b8e1ba13d21e19b2a3c021
roshankaranth <roshankaranth@gmail.com>
2025-03-25 10:59:42 +0530

    ALU

--- ALU.v
+++ ALU.v
@@ -40,23 +40,74 @@ module bit32_3to1mux(out,sel,in1,in2,in3);
     endgenerate
 endmodule
 
-module testbench();
-    reg [31:0] in1,in2,in3;
-    reg [1:0] sel;
-    wire [31:0] out;
+module bit32AND(out,in1,in2);
+    input wire [31:0] in1,in2;
+    output wire [31:0] out;
 
-    bit32_3to1mux uut(out,sel,in1,in2,in3);
-
-    initial
-        $monitor("time : %0t, in1 : %b, in2 = %b, in3 = %b, sel = %b, out = %b", $time,in1,in2,in3,sel,out);
-
-    initial begin
-        in1 = 32'b00000000_00000000_00000000_00000001;
-        in2 = 32'b00000000_00000000_00000000_00000011;
-        in3 = 32'b00000000_00000000_00000000_00000111;
-        #2
-        sel = 2'b00; #2;
-        sel = 2'b01; #2;
-        sel = 2'b10; #2;
-    end
+    assign out = in1 & in2;
 endmodule
+
+module bit32OR(out,in1,in2);
+    input wire [31:0] in1,in2;
+    output wire [31:0] out;
+
+    assign out = in1 | in2;
+endmodule
+
+module FA(Cout,Sum,In1,In2,Cin);
+    input wire [31:0] In1,In2;
+    input wire Cin;
+    output wire [31:0] Sum;
+    output wire Cout;
+
+    assign {Cout,Sum} = In1 + In2 + Cin;
+endmodule
+
+module ALU(in1,in2,Cin,Cout,op,binvert,out);
+    input wire [31:0] in1,in2;
+    input wire Cin,binvert;
+    output wire Cout;
+    output wire [31:0] out;
+    input wire [1:0] op;
+
+    wire [31:0] in2_val;
+
+    wire [31:0] OR_res;
+    wire [31:0] AND_res;
+    wire [31:0] FA_res;
+
+    assign in2_val = (binvert == 1'b0) ? in2 : ~in2;
+    assign out = (op == 2'b00) ? AND_res : (op == 2'b01) ? OR_res : FA_res;
+
+    FA m1(Cout,FA_res,in1,in2_val,(binvert == 1'b0) ? Cin : 1'b1);
+    bit32OR m2(OR_res, in1,in2);
+    bit32AND m3(AND_res, in1,in2);
+
+endmodule
+
+// module tbALU();
+//     reg Binvert, Carryin;
+//     reg [1:0] Operation;
+//     reg [31:0] a,b;
+//     wire [31:0] Result;
+//     wire CarryOut;
+
+//     ALU uut(a,b,Carryin,CarryOut,Operation,Binvert,Result);
+
+//     initial
+//         $monitor("$time : %0t, a = %b , b = %b, cin = %b, cout = %b, op = %b, binvert = %b, res = %b", $time,a,b,Carryin,CarryOut,Operation,Binvert,Result);
+
+//     initial begin
+//         a=32'ha5a5a5a5;
+//         b=32'h5a5a5a5a;
+//         Operation=2'b00;
+//         Binvert=1'b0;
+//         Carryin=1'b1; //must perform AND resulting in zero
+
+//         #100 Operation=2'b01; //OR
+//         #100 Operation=2'b10; //ADD
+//         #100 Binvert=1'b1;//SUB
+
+//         #200 $finish;
+//     end
+// endmodule

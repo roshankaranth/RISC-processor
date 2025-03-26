fd4c42e5797c923cb156b4758b114aae68a4765a
roshankaranth <roshankaranth@gmail.com>
2025-03-26 10:54:22 +0530

    controller correction

--- Controller.v
+++ Controller.v
@@ -1,8 +1,8 @@
 `timescale 1ns/1ps;
 
-module main_controller(RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp0, ALUop1, Op);
+module main_controller(RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp0, ALUOp1, Op);
     input wire [5:0] Op;
-    output wire RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp0, ALUop1;
+    output wire RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp0, ALUOp1;
     wire Rformat,lw,sw,beq;
 
     assign Rformat = (~Op[0]) & (~Op[1]) & (~Op[2]) & (~Op[3]) & (~Op[4]) & (~Op[5]);
@@ -18,7 +18,7 @@ module main_controller(RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Br
     assign MemWrite = sw;
     assign Branch = beq;
     assign ALUOp0 = Rformat;
-    assign ALUop1 = beq;
+    assign ALUOp1 = beq;
 
 endmodule
 
@@ -27,7 +27,7 @@ module ALU_controller(ALUOp0, ALUOp1, funct, operation);
     input wire [5:0] funct;
     output wire [2:0]operation;
 
-    assign operation[0] = (ALUOp0 & (funct[0] | funct[3]));
+    assign operation[0] = (ALUOp1 & (funct[0] | funct[3]));
     assign operation[1] = (~(ALUOp1) | ~(funct[2]));
     assign operation[2] = (ALUOp0 | (ALUOp1 & funct[1]));
 

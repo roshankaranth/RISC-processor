5d0dd6ba3b4576e024c300ce69a69903d6053431
roshankaranth <roshankaranth@gmail.com>
2025-03-30 18:18:03 +0530

    single cycle datapath

--- datapath.v
+++ datapath.v
@@ -1,108 +1,126 @@
 `timescale 1ns/1ps
 
-module program_counter(inp,clk,outp);
-    input wire [4:0] inp;
-    output reg [4:0] outp;
+module program_counter(in,out,clk);
+    input wire [4:0] in;
+    output wire [4:0] out;
+    reg [4:0] mem;
     input wire clk;
 
-    always @(negedge clk) begin
-        outp <= inp;
-    end
-endmodule;
+    // initial
+    //     mem = 5'b00000;
 
-module instruction_memory(pc,instr,clk,reset);
+    assign out = mem;
+
+    always @(negedge clk)begin
+        mem = in;
+    end
+endmodule
+
+module instruction_memory(instruction,address);
+    output wire [31:0] instruction;
     reg [31:0] mem [31:0];
-    input clk,reset;
-    input [4:0] pc;
-    output reg [31:0] instr;
+    input wire [4:0] address;
 
     initial begin
-        mem[0] = 32'h00622020;
-        mem[1] = 32'h8C450BB8;
-        mem[2] = 32'h1009000A;
-        mem[3] = 32'h01A00008;
-        mem[4] = 32'h12345678;
+        mem[0] = 32'h00622020; //add $a0, $a2, $a0
+        mem[1] = 32'h8C450BB8; //lw $t2, 3000($v0)
+        mem[2] = 32'h1009000A; //beq $zero, $t1, 10
+        mem[3] = 32'h01A00008; //jr $t5
+        mem[4] = 32'h12345678; //beq $s1, $v1, 0x5678
         mem[5] = 32'h98765432;
-        mem[6] = 32'hABCDABCD; 
+        mem[6] = 32'hABCDABCD;
     end
 
-    always @(negedge clk, pc) begin
-        if(reset) instr = 32'b0;
-        else instr = mem[pc];
-    end
+    assign instruction = mem[address];
 endmodule
 
-module data_memeory(addr, wr_data, rd_data, clk, reset, memwrite, memread);
-    input clk,reset,memread,memwrite;
-    input [31:0] wr_data;
-    input [4:0] addr;
-    output reg [31:0] rd_data;
-    reg [31:0] mem [31:0];
-    integer j;
+// module data_memory(memread,memwrite,address,clk,wr_data,rd_data);
+//     input wire memread,memwrite,clk;
+//     output wire [4:0] address;
+//     input wire [31:0] wr_data;
+//     output reg [31:0] rd_data;
 
-    initial begin
-        for(j = 0; j < 32 ; j = j + 1) begin
-            mem[j] = 32'b0;
-        end
-    end
+//     reg[31:0]data_memory [31:0];
+//     integer j;
 
-    always @(negedge clk) begin
-        if(reset) rd_data = 32'b0;
-        else begin
-            if(memread) rd_data = mem[addr];
-            else if(memwrite) mem[addr] = wr_data;
-        end
-    end
-endmodule
+//     initial begin
+//         for(j=0;j<32;j=j+1) begin
+//             data_memory[j] = 32'b0;
+//         end
+//     end
 
-module bit32_2to1mux(out,in1,in2,sel);
-    input wire [31:0] in1,in2;
-    input wire sel;
+//     always @(negedge clk) begin
+//         if(memread) rd_data = data_memory[address];
+//         if(memwrite) data_memory[address] = wr_data;
+//     end
+// endmodule
+
+// module bit32_2to1mux(in1, in1, sel,out);
+//     input wire [31:0] in1,in2;
+//     input wire sel;
+//     output wire [31:0] out;
+
+//     assign out = (sel) ? in2 : in1;
+// endmodule
+
+// module signExt(in,out);
+//     input wire [15:0] in;
+//     output wire [31:0] out;
+
+//     assign out = {(in[15]?16'b1:16'b0),in};
+// endmodule
+
+// module shifter(in,out);
+//     input wire [25:0] in;
+//     output wire [27:0] out;
+
+//     assign out = {in,2'b00};
+// endmodule
+
+module SCDatapath(clk,address,out,reset);
+    input wire clk;
+    input wire [4:0] address;
+    input wire reset;
+    wire [4:0] pc;
+    wire [31:0] instruction;
+    wire RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp0, ALUOp1;
+    wire [2:0]operation;
+    wire [31:0] ReadData1, ReadData2;
+    wire Cout;
     output wire [31:0] out;
 
-    assign out = (sel) ? in2 : in1;
-endmodule
-
-module signExt(inp, out);
-    input wire [15:0] inp;
-    input wire [31:0] out;
-
-    assign out = {(inp[15])?16'b1:16'b0,inp};
     
+    program_counter p1(address,pc,clk);
+    instruction_memory im(instruction,pc);
+    main_controller mc(RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp0, ALUOp1, instruction[31:26]);
+    ALU_controller ac(ALUOp0, ALUOp1, instruction[5:0], operation);
+    RegFile rf(clk,reset,instruction[25:21],instruction[20:16],out,instruction[15:11],RegWrite,ReadData1,ReadData2);
+    ALU alu(ReadData1,ReadData2,1'b0,Cout,operation,1'b0,out);
+
 endmodule
 
-module shifter(inp,out);
-    input wire [31:0] inp;
-    output wire [31:0] out;
-
-    assign out = {inp[29:0],2'b00};
-endmodule
-
-module SCDatapath(input reset);
+module testbench();
     reg clk;
-    reg [4:0] pcOut;
+    reg [4:0] address;
+    reg reset;
+    wire [31:0] out;
+    
 
-    program_counter pc(5'b0,clk,reset);
-    instruction_memory im(pc,reset,instr);
-endmodule
+    SCDatapath uut(clk,address,out,reset);
 
-module TestBench;
-    wire [31:0] ALU_output;
-    reg [31:0] PC;
-    reg reset,clk;
+    initial
+        $monitor("time : %0t, address : %b, out : %b",$time,address,out);
 
-    SCDataPath SCDP(ALU_output,PC,reset,clk);
     initial begin
-    
-    $monitor("at time %0d IPC = %d, Reset = %d , CLK = %d , ALU Output = %d",$time,start_pc,rst,clk, ALU_output);
-    
-    #0 clk = 0; PC = 5;
-    #120 rst = 1;
-    #400 $stop;
-    
+        forever
+            #5 clk = ~clk;
     end
-    
-    always begin
-    #20 clk = ~clk;
+
+    initial begin
+        clk = 1; address = 5'b00000; reset = 0;
+        #6 address = 5'b00001; reset = 1;
+        #20 address = 5'b00011;
+        #40 $finish;
     end
 endmodule
+
